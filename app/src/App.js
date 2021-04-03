707a28f18a5b1aa9f529fe8b192817a6d90b9a12
ali-alzyod <ali198724@gmail.com>
2021-04-03 22:50:32 +0300

    open file from device

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useRef, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Player } from './Components/Player';
 import videoSample from './assets/sample.webm'
 import filterSample from './assets/filter.txt'
@@ -7,13 +7,19 @@ import Menu from './Components/Menu'
 import List from './Components/List'
 
 function App() {
+  const [videoSrc, setVideoSrc] = useState(videoSample)
+  useEffect(() => {
+    window.setVideoSrc = setVideoSrc;
+  })
   return (
     <div className="App">
-      <Menu/>
+      <Menu />
       <div style={{ width: '100%', margin: '0 auto', marginTop: '32px' }}>
-        <Player videoSrc={videoSample} filterSrc={filterSample} />
-        <div style={{position: "absolute", top:"80px", right:"60px", backgroundColor:"transparent"}}>
-        <List/>
+        <Player
+          videoSrc={videoSrc}
+          filterSrc={filterSample} />
+        <div style={{ position: "absolute", top: "80px", right: "60px", backgroundColor: "transparent" }}>
+          <List />
         </div>
       </div>
     </div>
--- app/src/Components/Menu/Menu.js
+++ app/src/Components/Menu/Menu.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import './menu.css'
 
 export default function Menu() {
+    const fileInput = useRef(null)
+    const openVideo = (e) => {
+        localStorage.url = e.target.value;
+        window.setVideoSrc(URL.createObjectURL(e.target.files[0]));
+    }
     return (
-        <header>
-            <ul className='horizontal'>
-                <li className='horizontal'>File</li>
-                <li className='horizontal'>Edit</li>
-            </ul>
-        </header>
+        <div className="navbar">
+            <div className="dropdown">
+                <button className="dropbtn">File 
+                </button>
+                <div className="dropdown-content">
+                    <input ref={fileInput} type='file' onChange={openVideo}/>
+                    <a href="#" onClick={()=>fileInput.current.click()}>Open video</a>
+                    <a href="#">Open filter</a>
+                    <a href="#">Open subtitle</a>
+                </div>
+            </div> 
+            </div>
     )
 }
