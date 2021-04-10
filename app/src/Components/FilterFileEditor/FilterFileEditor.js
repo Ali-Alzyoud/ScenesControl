1fe8002012021604b91407ed8423f69f9ffbc7b4
ali-alzyod <ali198724@gmail.com>
2021-04-10 04:58:46 +0300

    Component for record

--- app/src/Components/FilterFileEditor/FilterFileEditor.js
+++ app/src/Components/FilterFileEditor/FilterFileEditor.js
@@ -1,61 +1,14 @@
 import React from 'react'
+import FilterRecord from './FilterRecord'
 import './style.css'
 
-export default function FilterFileEditor({sceneObject}) {
+export default function FilterFileEditor({ sceneObject }) {
     return (
         <div>
             {
-                sceneObject.Records.map(record => {
-                    var fromDates = record.From.split(":");
-                    var toDates = record.To.split(":");
-                    return (
-                        <tr>
-                            <td>
-                                <input className='bk' value={fromDates[0]} />&nbsp;:&nbsp;
-                                <input className='bk' value={fromDates[1]} />&nbsp;:&nbsp;
-                                <input className='bkLong' value={fromDates[2]} />
-                            </td>
-                            <div className='break'></div>
-                            <td>
-                                <input className='bk' value={toDates[0]} />&nbsp;:&nbsp;
-                                <input className='bk' value={toDates[1]} />&nbsp;:&nbsp;
-                                <input className='bkLong' value={toDates[2]} />
-                            </td>
-                            <td>
-                                <select name='Type'>
-                                    {/* {
-    for (var key in SceneType) {
-        var value = SceneType[key];
-        if (typeof (value) == "string") {
-                        str += "<option value='" + value + "'";
-            if (value == sceneObject.Type) {
-                        str += " selected"
-                    }
-            str += ">" + value + "</option>";
-        }
-    }
-} */}
-                                </select>
-                            </td>
-                            <td>
-                                <select name='Age'>
-                                    {/* for (var key in SceneIntensity) {
-        var value = SceneIntensity[key];
-        if (typeof (value) == "string") {
-            str += "<option value='" + value + "'";
-            if (value == sceneObject.Age) {
-                str += " selected"
-            }
-            str += ">" + value + "</option>";
-        }
-    } */}
-                                </select>
-                            </td >
-                        </tr >
-                    )
-
-                }
-                )
+                sceneObject.Records.map((record, index) => {
+                    return <FilterRecord key={index} record={record} />
+                })
             }
         </div>
     )
