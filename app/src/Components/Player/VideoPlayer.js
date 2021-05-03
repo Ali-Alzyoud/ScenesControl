34509de05ce2449f5cc724cd7366d5856f2f5336
Ali.Alzyod <ali198724@gmail.com>
2021-05-03 23:24:45 +0300

    update video controls

--- app/src/Components/Player/VideoPlayer.js
+++ app/src/Components/Player/VideoPlayer.js
@@ -107,7 +107,7 @@ class VideoPlayer extends React.Component {
             position: "relative",
             gridTemplateRows: "60% 20% 20%",
             position: "relative",
-            top: "-200%",
+            top: "-100%",
             left: 0,
             width: "100%",
             height: "100%",
