3bc44f24e305b697b786115abcf30af9db12c012
Ali Mahmoud <ali98724@gmail.com>
2021-06-05 14:33:40 +0300

    enhance about dialog

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,8 +5,26 @@ export default function About({close}) {
     return (
         <div className='overlay'>
             <div className='box'>
-                <h1> Ali Alzyod </h1>
-                <h1> 2021 </h1>
+                <h1> Guided Scene Player/Editor </h1>
+                <p> Creator : Ali M Alzyod </p>
+                <p> Email : ali198724@gmail.com </p>
+                <p> Date : 2021 </p>
+                <br/>
+                <p> Key Bindings </p>
+                <div className='part'>
+                    e : open Editor <br/>
+                    c : open configure
+                </div>
+                <div className='part'>
+                    [ : from time <br/>
+                    ] : to time
+                </div>
+                <div className='part'>
+                    n : new record <br/>
+                    s : select record
+                </div>
+                <br/>
+                <br/>
                 <button className='button' onClick={close}>Ok</button>
             </div>
         </div>
