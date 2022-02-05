b05e61b681572c5d0329d1bdc93571ad38b4795f
Ali Mahmoud <ali98724@gmail.com>
2022-02-05 17:03:12 +0200

    enhance filter

--- src/Components/Player/VideoFilter.js
+++ src/Components/Player/VideoFilter.js
@@ -6,14 +6,14 @@ import { setMute, setTime, setSpeed } from "../../redux/actions";
 import { PLAYER_ACTION } from '../../redux/actionTypes';
 
 
-var FilterStyle = {
-  pointerEvents: "none",
-  position: "absolute",
-  left: "0",
-  top: "0",
-  width: "100%",
-  height: "100%",
-  zIndex: 2,
+const FilterStyle = {
+    pointerEvents: "none",
+    position: "absolute",
+    left: 0,
+    top: 0,
+    width: "100%",
+    height: "100%",
+    zIndex: 2,
 };
 
 function VideoFilter({
@@ -25,90 +25,90 @@ function VideoFilter({
     playerConfig,
     blackScreen
 }) {
-  const [style, setStyle] = useState(FilterStyle);
+    const [style, setStyle] = useState(FilterStyle);
 
-  useEffect(() => {
-      if (blackScreen) {
-          setStyle({ ...FilterStyle, background: "black" });
-          return;
-      }
-    if (!records || !records.length > 0) return;
-
-    var currentRecords = getRecordsAtTime(time);
-    var mute = false;
-    var skip = false;
-    var black = false;
-    var blur = false;
-    var blurExtra = false;
-    var blurExtreme = false;
-    var doubleSpeed = false;
-    var skipRecord = null;
-
-    for (var i = 0; i < currentRecords.length; i++) {
-        var record = currentRecords[i];
-        if(playerConfig[record.Type][0] === PLAYER_ACTION.MUTE || playerConfig[record.Type][1] === PLAYER_ACTION.MUTE) {
-            mute = true;
-        }
-        if(playerConfig[record.Type][0] === PLAYER_ACTION.SKIP || playerConfig[record.Type][1] === PLAYER_ACTION.SKIP) {
-            skip = true;
-            skipRecord = record;
-        }
-        if(playerConfig[record.Type][0] === PLAYER_ACTION.BLACK || playerConfig[record.Type][1] === PLAYER_ACTION.BLACK) {
-            black = true;
-        }
-        if(playerConfig[record.Type][0] === PLAYER_ACTION.BLUR || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR) {
-            blur = true;
-        }
-        if(playerConfig[record.Type][0] === PLAYER_ACTION.BLUR_EXTRA || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR_EXTRA) {
-            blurExtra = true;
-        }
-        if(playerConfig[record.Type][0] === PLAYER_ACTION.BLUR_EXTREME || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR_EXTREME) {
-            blurExtreme = true;
-        }
-        if(playerConfig[record.Type][0] === PLAYER_ACTION.BLUR_EXTREME_X2 || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR_EXTREME_X2) {
-            blurExtreme = true;
-            doubleSpeed = true;
-        }
-    }
-
-    setMute(mute);
-
-    if (skip){
-        setTime(skipRecord.endTime() + 0.01);
-        return;
-    }
-
-    if (blur || black|| blurExtra || blurExtreme) {
-        if(blur)
-            setStyle({ ...FilterStyle, backdropFilter: "blur(15px)" });
-        else if(blurExtra)
-            setStyle({ ...FilterStyle, backdropFilter: "blur(45px)" });
-        else if(blurExtreme)
-            setStyle({ ...FilterStyle, backdropFilter: "blur(80px)" });
-        else
+    useEffect(() => {
+        if (blackScreen) {
             setStyle({ ...FilterStyle, background: "black" });
-    }
-    else {
-        setStyle({ ...FilterStyle});
-    }
+            return;
+        }
+        if (!records || !records.length) return;
 
-    if(doubleSpeed){
-        setSpeed(2.0);
-    } else {
-        setSpeed(1.0);
-    }
+        var currentRecords = getRecordsAtTime(time);
+        var mute = false;
+        var skip = false;
+        var black = false;
+        var blur = false;
+        var blurExtra = false;
+        var blurExtreme = false;
+        var doubleSpeed = false;
+        var skipRecord = null;
 
-  }, [time, records, playerConfig, blackScreen]);
+        for (var i = 0; i < currentRecords.length; i++) {
+            var record = currentRecords[i];
+            if (playerConfig[record.Type][0] === PLAYER_ACTION.MUTE || playerConfig[record.Type][1] === PLAYER_ACTION.MUTE) {
+                mute = true;
+            }
+            if (playerConfig[record.Type][0] === PLAYER_ACTION.SKIP || playerConfig[record.Type][1] === PLAYER_ACTION.SKIP) {
+                skip = true;
+                skipRecord = record;
+            }
+            if (playerConfig[record.Type][0] === PLAYER_ACTION.BLACK || playerConfig[record.Type][1] === PLAYER_ACTION.BLACK) {
+                black = true;
+            }
+            if (playerConfig[record.Type][0] === PLAYER_ACTION.BLUR || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR) {
+                blur = true;
+            }
+            if (playerConfig[record.Type][0] === PLAYER_ACTION.BLUR_EXTRA || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR_EXTRA) {
+                blurExtra = true;
+            }
+            if (playerConfig[record.Type][0] === PLAYER_ACTION.BLUR_EXTREME || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR_EXTREME) {
+                blurExtreme = true;
+            }
+            if (playerConfig[record.Type][0] === PLAYER_ACTION.BLUR_EXTREME_X2 || playerConfig[record.Type][1] === PLAYER_ACTION.BLUR_EXTREME_X2) {
+                blurExtreme = true;
+                doubleSpeed = true;
+            }
+        }
 
-  return <div style={style}></div>;
+        setMute(mute);
+
+        if (skip) {
+            setTime(skipRecord.endTime() + 0.01);
+            return;
+        }
+
+        if (blur || black || blurExtra || blurExtreme) {
+            if (blur)
+                setStyle({ ...FilterStyle, backdropFilter: "blur(15px)" });
+            else if (blurExtra)
+                setStyle({ ...FilterStyle, backdropFilter: "blur(45px)" });
+            else if (blurExtreme)
+                setStyle({ ...FilterStyle, backdropFilter: "blur(80px)" });
+            else
+                setStyle({ ...FilterStyle, background: "black" });
+        }
+        else {
+            setStyle({ ...FilterStyle });
+        }
+
+        if (doubleSpeed) {
+            setSpeed(2.0);
+        } else {
+            setSpeed(1.0);
+        }
+
+    }, [time, records, playerConfig, blackScreen]);
+
+    return <div style={style}></div>;
 }
 
 
 const mapStateToProps = state => {
     const records = selectRecords(state);
     const time = selectTime(state);
-    const playerConfig =  selectPlayerConfig(state);
+    const playerConfig = selectPlayerConfig(state);
     return { records, time, playerConfig };
-  };
+};
 
 export default connect(mapStateToProps, { setMute, setTime, setSpeed })(VideoFilter);
