cb36934e8b5bcc90c2d8bcad02645c7f0d043127
Ali Mahmoud <ali98724@gmail.com>
2022-04-09 16:08:44 +0300

    implement streaming

--- src/common/API/API.js
+++ src/common/API/API.js
@@ -1,6 +1,7 @@
 export const endpoint = 'https://raw.githubusercontent.com/Ali-Alzyoud/MediaFilterFiles/main/Media';
 const URLS = {
-    RECORDS : endpoint+'/mediaRecords.json'
+    RECORDS : endpoint+'/mediaRecords.json',
+    STREAMS : '18.159.209.161/s3Proxy'
 }
 
 
@@ -11,4 +12,28 @@ export async function getMediaRecords(){
     const status = response.status;
     const result = await response.json();
     return result;
+}
+
+export async function getStream(videoFile){
+    let response; 
+    let blob;
+    try{
+    response = await fetch(URLS.STREAMS,{
+        method: 'GET'
+    });
+    }
+    catch(ex){
+        console.log(ex)
+    }
+    const status = response.status;
+    try{
+        blob = await response.blob();
+    }
+    catch(ex){
+        console.log(ex)
+    }
+    const newBlob = new Blob([blob]);
+    const blobUrl = window.URL.createObjectURL(newBlob);
+
+    return blobUrl;
 }
