09f8cfa18a0139ce0a3b12a366e9e918bda04815
Ali Mahmoud <ali98724@gmail.com>
2022-05-01 00:45:13 +0300

    sync two subtitle

--- src/Components/SubtitleEditor/SubtitleEditor.js
+++ src/Components/SubtitleEditor/SubtitleEditor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { FaSave, FaPlus, FaMinus } from 'react-icons/fa'
 import { connect, useDispatch, useSelector } from "react-redux";
 import { setSettings_syncConfig } from '../../redux/actions';
@@ -23,6 +23,35 @@ function SubtitleEditor(props) {
         // element.download = videoName+".txt";
         // document.body.appendChild(element); // Required for this to work in FireFox
         // element.click();
+    }
+
+    const sync = () => {
+        if(subtitleRecords1.length == 1 && subtitleRecords2.length ==1){
+            const recordSrc = subtitleRecords1[0];
+            const recordDst = subtitleRecords2[0];
+            const time = (recordDst.from - recordSrc.from);
+            dispatch(setSettings_syncConfig({
+                ...syncConfig,
+                subtitleDelay: time/1000,
+                subtitleSlope: 1,
+            }));
+        } else if(subtitleRecords1.length == 2 && subtitleRecords2.length == 2){
+            const recordSrc1From = Math.min(subtitleRecords1[0].from,subtitleRecords1[1].from);
+            const recordSrc2From = Math.max(subtitleRecords1[0].from,subtitleRecords1[1].from);
+            const recordDst1From = Math.min(subtitleRecords2[0].from,subtitleRecords2[1].from);
+            const recordDst2From = Math.max(subtitleRecords2[0].from,subtitleRecords2[1].from);
+            const timeDurationSrc = Math.abs(recordSrc1From - recordSrc2From);
+            const timeDurationDst = Math.abs(recordDst1From - recordDst2From);
+            const slope = timeDurationDst/timeDurationSrc;
+            const delay = recordDst1From - slope * recordSrc1From;
+            dispatch(setSettings_syncConfig({
+                ...syncConfig,
+                subtitleDelay: delay/1000,
+                subtitleSlope: slope,
+            }));
+        } else {
+            alert("Select 1 or 2 records to sync them")
+        }
     }
 
     const slopeInc = () => {
@@ -62,6 +91,32 @@ function SubtitleEditor(props) {
     }
 
     const hasSyncSubtitleFile = !!(subtitleSync && subtitleSync.length > 0);
+    const [subtitleRecords1, setSubtitleRecords1] = useState([]);
+    const [subtitleRecords2, setSubtitleRecords2] = useState([]);
+
+    const onCheckSubtitle1 = useCallback( (record, checked) => {
+        if(checked){
+            setSubtitleRecords1(prev => [...prev,record])
+        } else {
+            setSubtitleRecords1(subtitleRecords1.filter((item) => {
+                return item.from != record.from
+            }));
+        }
+      },[]);
+
+    const onCheckSubtitle2 = useCallback(
+        (record, checked) => {
+          if(checked){
+            setSubtitleRecords2(prev => [...prev,record])
+          } else {
+              setSubtitleRecords2(subtitleRecords2.filter((item) => {
+                  return item.from != record.from
+              }));
+          }
+        },
+        [],
+      )
+
     return (
         <div className='editor-container'>
             <div className='container' onClick={saveItems}>
@@ -75,7 +130,6 @@ function SubtitleEditor(props) {
             <div className='container' onClick={delayDec}>
                 <FaMinus className='middle' />
             </div>
-            <br /><br />
             <div className='container' onClick={slopeInc}>
                 <FaPlus className='middle' />
             </div>
@@ -84,6 +138,9 @@ function SubtitleEditor(props) {
                 <FaMinus className='middle' />
             </div>
             <br /><br />
+            <div className='container rect' onClick={sync}>
+                <span className='middle'>Sync</span>
+            </div>
             <div className='table-container'>
                 <table style={{ float: hasSyncSubtitleFile ? 'left' : 'unset', marginRight: '20px' }}>
                     <tr>
@@ -93,7 +150,7 @@ function SubtitleEditor(props) {
                     </tr>
                     {
                         subtitle.map((record, index) => {
-                            return <SubtitleRecord record={record} />
+                            return <SubtitleRecord record={record} onCheck={onCheckSubtitle1}/>
                         })
                     }
                 </table>
@@ -106,7 +163,7 @@ function SubtitleEditor(props) {
                         </tr>
                         {
                             subtitleSync.map((record, index) => {
-                                return <SubtitleRecord record={record} dontChange={true}/>
+                                return <SubtitleRecord record={record} dontChange={true} onCheck={onCheckSubtitle2}/>
                             })
                         }
                     </table>}
--- src/Components/SubtitleEditor/SubtitleRecord.js
+++ src/Components/SubtitleEditor/SubtitleRecord.js
@@ -1,17 +1,30 @@
-import React, { memo, Fragment } from 'react'
+import React, { memo, Fragment, useState, useCallback } from 'react'
 import { useSelector } from 'react-redux';
 import { getSyncConfig } from '../../redux/selectors';
 
 const timeToString = (time) => {
-    const hh = String(Math.floor(time / 60 / 60 / 1000)).padStart(2,0);
-    const mm = String(Math.floor(time / 60 / 1000)).padStart(2,0);
-    const ss = String(Math.floor(time / 1000)).padStart(2,0);
-    const ms = String(Math.floor(time % 1000)).padStart(3,0);
+    const hh = String(Math.floor(time / 60 / 60 / 1000)).padStart(2, 0);
+    const mm = String(Math.floor(time / 60 / 1000)).padStart(2, 0)%60;
+    const ss = String(Math.floor(time / 1000)).padStart(2, 0)%60;
+    const ms = String(Math.floor(time % 1000)).padStart(3, 0);
 
     return hh + ":" + mm + ":" + ss + "." + ms;
 }
 
-function SubtitleRecord({record, dontChange}) {
+function SubtitleRecord({ record, dontChange, onCheck }) {
+    const [checked, setChecked] = useState(false);
+    const onChange = useCallback(
+        () => {
+            setChecked((prev)=>{
+                if (onCheck) {
+                    onCheck(record, !prev);
+                }
+                return !prev;
+            })
+        },
+        [],
+    )
+
     const time = useSelector(state => {
         const delay = getSyncConfig(state).subtitleDelay;
         return delay;
@@ -21,23 +34,24 @@ function SubtitleRecord({record, dontChange}) {
         return slope;
     });
     return (
-        <tr key={record.from + "_" +time}>
+        <tr key={record.from + "_" + time}>
             <td>
                 {timeToString(dontChange ? record.from : (record.from * slope + time * 1000))}
             </td>
             <td>
-                {timeToString(dontChange ? record.to: (record.to * slope + time * 1000))}
+                {timeToString(dontChange ? record.to : (record.to * slope + time * 1000))}
             </td>
             <td>
                 <p>
-                {record.content.map((txt, index) => {
-                    return <Fragment>
-                        {index > 0 && <br/>}
-                        <span>{txt}</span>
-                         </Fragment>
-                })}
+                    {record.content.map((txt, index) => {
+                        return <Fragment>
+                            {index > 0 && <br />}
+                            <span>{txt}</span>
+                        </Fragment>
+                    })}
                 </p>
             </td>
+            <td><input type='checkbox' onChange={onChange} checked={checked}></input></td>
         </tr >
     )
 }
