ecec31ac0f0310106f13a7ea2dbaebb435670987
Ali Alzyod <ali.alzyod@tuhoon.com>
2024-02-24 19:44:23 +0300

    update

---
+++ src/Components/FilterPicker2/FilterPicker.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import { MdClose, MdSearch } from 'react-icons/md'
+import FileRecord from './FileRecord'
+import * as API from '../../common/API/API'
+
+import { connect } from "react-redux";
+import { setFilterItems, setSubtitle, setModalOpen, setVideoSrc, setVideoName, setDuration, setTime } from '../../redux/actions'
+
+import "./style.css"
+
+function FilterPicker({
+    close,
+    setFilterItems,
+    setSubtitle,
+    setVideoSrc,
+    setVideoName,
+    setModalOpen,
+    setDuration,
+    setTime,
+    folders,
+    path,
+}) {
+    const [recordsItems, setRecordsItems] = useState([]);
+    useEffect(() => {
+        API.getMediaRecords().then((value) => {
+            const fileRecords = [];
+            setRecordsItems(value.records);
+        });
+        setModalOpen(true);
+        return () => {
+            setModalOpen(false);
+        }
+    }, []);
+
+    const copy = ({image,video,srt,filter}) => {
+        setVideoSrc(video);
+        setVideoName(video);
+        close();
+    }
+    return (
+        <div className="filters-container">
+            <div className="filters-container-body">
+                <MdClose className="filters-container-close" onClick={close} />
+                <div className='filters-container-input-container'>
+                    {/* <input className='filters-container-input' onChange={textChanged}></input> */}
+                </div>
+                <MdSearch className="filters-container-search" />
+                <div className="filter-files">
+                    <list>
+                    {folders.map((item => {
+                        let image = item.files.filter(file => file.includes(".jpg")||file.includes(".png"))?.[0]
+                        let video = item.files.filter(file => file.includes(".mkv")||file.includes(".mp4"))?.[0]
+                        let srt = item.files.filter(file => file.includes(".srt"))?.[0]
+                        let filter = item.files.filter(file => file.includes(".txt"))?.[0]
+                        image = `${path}/${item.folder}/${image}`;
+                        video = `${path}/${item.folder}/${video}`;
+                        srt = `${path}/${item.folder}/${srt}`;
+                        filter = `${path}/${item.folder}/${filter}`;
+
+                        return  <FileRecord
+                        imgSrc={image}
+                        title={item.folder}
+                        copy={()=>copy({image,video,srt,filter})}
+                        />
+                    }))}
+                    </list>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default connect(
+    null,
+    { setSubtitle, setFilterItems, setModalOpen, setVideoSrc, setVideoName, setDuration, setTime }
+)(FilterPicker);
+
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -4,17 +4,18 @@ import { SceneGuideClass } from '../../common/SceneGuide'
 import About from '../About'
 import Settings from '../Settings'
 import FilterPicker from '../FilterPicker'
+import FilterPicker2 from '../FilterPicker2'
 
 
 import { connect } from "react-redux";
-import { setFilterItems, setVideoSrc, setSubtitle,setSubtitleSync, setVideoName, setTime, setDuration } from '../../redux/actions'
+import { setFilterItems, setVideoSrc, setSubtitle, setSubtitleSync, setVideoName, setTime, setDuration } from '../../redux/actions'
 
 import './menu.css'
 
 function Menu({ setFilterItems, setVideoSrc, setVideoName, setSubtitle, setSubtitleSync, setTime, setDuration }) {
     const videoInput = useRef(null);
     const subtitleInput = useRef(null);
-    const subtitleSyncInput= useRef(null);
+    const subtitleSyncInput = useRef(null);
     const filterInput = useRef(null);
     const videoInputURL = useRef(null);
     const subtitleInputURL = useRef(null);
@@ -24,7 +25,14 @@ function Menu({ setFilterItems, setVideoSrc, setVideoName, setSubtitle, setSubti
     const [about, setabout] = useState(false);
     const [settings, setSettings] = useState(false);
     const [filterPicker, setfilterPicker] = useState(false);
-    const [files, setFiles] = useState({});
+    const [filterPicker2, setfilterPicker2] = useState(false);
+    const [folders, setFolders] = useState([]);
+    const [path, setPath] = useState("")
+
+
+    const inputRef = useRef();
+    const inputPathRef = useRef();
+
     const openVideoFile = (e) => {
         if (e.target.files.length < 1) return;
 
@@ -63,7 +71,7 @@ function Menu({ setFilterItems, setVideoSrc, setVideoName, setSubtitle, setSubti
     useEffect(() => {
         fetch('videos.json').then(res => {
             res.json().then(content => {
-                setFiles(content);
+                // setFiles(content);
             });
         });
     }, [])
@@ -87,22 +95,25 @@ function Menu({ setFilterItems, setVideoSrc, setVideoName, setSubtitle, setSubti
                 setSubtitle(records);
             });
         }
-        if (subSyncURL){
+        if (subSyncURL) {
             SrtClass.ReadFile(subSyncURL).then((records) => {
                 setSubtitleSync(records);
             });
         }
     }
+
+    const [remoteMeta, setRemoteMeta] = useState(localStorage.getItem("remoteMeta") || "http://127.0.0.1:4001/api/v1/files");
+    const [remotePath, setRemotePath] = useState(localStorage.getItem("remotePath") || "http://127.0.0.1:8061");
     return (
-        <div className="navbar">
+        <div className="navbar" style={{ display: 'flex' }}>
             <div className="dropdown">
                 <button className="dropbtn">File
                 </button>
                 <div className="dropdown-content">
-                    <input className='hidden' key={key+"_1"} ref={videoInput} type='file' onChange={openVideoFile} />
-                    <input className='hidden' key={key+"_2"} ref={subtitleInput} type='file' onChange={openSubtitleFile} />
-                    <input className='hidden' key={key+"_2"} ref={subtitleSyncInput} type='file' onChange={openSubtitleFileSync} />
-                    <input className='hidden' key={key+"_3"} ref={filterInput} type='file' onChange={openFilterFile} />
+                    <input className='hidden' key={key + "_1"} ref={videoInput} type='file' onChange={openVideoFile} />
+                    <input className='hidden' key={key + "_2"} ref={subtitleInput} type='file' onChange={openSubtitleFile} />
+                    <input className='hidden' key={key + "_2"} ref={subtitleSyncInput} type='file' onChange={openSubtitleFileSync} />
+                    <input className='hidden' key={key + "_3"} ref={filterInput} type='file' onChange={openFilterFile} />
                     <a href="#" onClick={() => videoInput.current.click()}>Open video</a>
                     <a href="#" onClick={() => subtitleInput.current.click()}>Open subtitle</a>
                     <a href="#" onClick={() => subtitleSyncInput.current.click()}>Open subtitle Sync</a>
@@ -141,9 +152,44 @@ function Menu({ setFilterItems, setVideoSrc, setVideoName, setSubtitle, setSubti
                 <button className="dropbtn" onClick={() => { setabout(true) }}>About
                 </button>
             </div>
+            <div className="dropdown" style={{ alignSelf: "center" }}>
+                <div style={{display:'flex', flexDirection:'column'}}>
+                <input value={remoteMeta} onChange={(e)=>{
+                    setRemoteMeta(e.target.value);
+                    localStorage.setItem("remoteMeta",e.target.value)
+                }} ref={inputRef} placeholder='Meta'/>
+                <input value={remotePath}  onChange={(e)=>{
+                    setRemotePath(e.target.value);
+                    localStorage.setItem("remotePath",e.target.value)
+                }} ref={inputPathRef} placeholder='Path'/>
+                </div>
+                <button onClick={async () => {
+                    const url = inputRef.current.value;
+                    const path = inputPathRef.current.value;
+                    try {
+                        const response = await fetch(url, {
+                            method: "GET",
+                            headers: {
+                                accept: "application/json",
+                            }
+                        });
+                        const folders = await response.json();
+                        setFolders(folders.files);
+                        setPath(path);
+                        setfilterPicker2(true);
+                    } catch (error) {
+                        alert(error.message)
+                    }
+
+                    // const data = await response.json();
+                    // setFiles(data);
+                    // setfilterPicker2(true);
+                }}>Show Store</button>
+            </div>
             {about && <About close={() => { setabout(false) }} />}
             {settings && <Settings close={() => { setSettings(false) }} />}
             {filterPicker && <FilterPicker close={() => { setfilterPicker(false) }} />}
+            {filterPicker2 && <FilterPicker2 folders={folders} path={path} close={() => { setfilterPicker2(false) }} />}
         </div>
     )
 }
